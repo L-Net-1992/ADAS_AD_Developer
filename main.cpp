ec898fb9d610561c21452760aa2094055cead4aa
fc <fengchong@126.com>
2021-12-24 11:29:46 +0800

    修改所有ui的字体

--- main.cpp
+++ main.cpp
@@ -8,6 +8,11 @@ int main(int argc, char *argv[])
     //此处引入plugins文件夹中的库文件，当前用来支持Sqlite
     QApplication::addLibraryPath("./plugins");
     QApplication a(argc, argv);
+    QFont font;
+    font.setPointSize(9);
+    font.setFamily("黑体");
+    a.setFont(font);
+
     MainWindow w;
     w.show();
     return a.exec();
